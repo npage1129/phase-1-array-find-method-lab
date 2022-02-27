d0b755e17d4cfaa53e2fa0120b54c3a972135525
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-02-27 16:26:52 -0700

    commit tree d4e527f0019bb8046d2f960e89421c7b2ef7fa32

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// //
+// const superbowlWin = array => (array === "W")? array.year : undefined;
+// console.log(record.find(superbowlWin))
+ 
+function superbowlWin(array){
+    let conclusion = undefined
+    for(const game of array){
+        if(game.result === "W"){
+             conclusion = game.year;
+            break;
+
+        }
+
+    }
+   return conclusion; 
+}
+superbowlWin.find(record);
